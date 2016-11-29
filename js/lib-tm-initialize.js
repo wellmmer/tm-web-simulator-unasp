1f27ec86b5b0aaf00b6ce01fc4be0e366a2d6477
wellmmeR <wellmmer@gmail.com>
2016-11-29 01:14:04 -0200

    Carousel e JQuery atualizados e funcionando. Prototype nativo alterado!

--- js/lib-tm-initialize.js
+++ js/lib-tm-initialize.js
@@ -1,36 +1,4 @@
 window.onload = function() {
-    jQuery(function($) {
-
-        // settings
-        var $slider = $('.slider'); // class or id of carousel slider
-        var $slide = 'li'; // could also use 'img' if you're not using a ul
-        var $transition_time = 1000; // 1 second
-        var $time_between_slides = 4000; // 4 seconds
-
-        function slides() {
-            return $slider.find($slide);
-        }
-
-        slides().fadeOut();
-
-        // set active classes
-        slides().first().addClass('active');
-        slides().first().fadeIn($transition_time);
-
-        // auto scroll 
-        $interval = setInterval(
-            function() {
-                var $i = $slider.find($slide + '.active').index();
-
-                slides().eq($i).removeClass('active');
-                slides().eq($i).fadeOut($transition_time);
-
-                if (slides().length == $i + 1) $i = -1; // loop to start
-
-                slides().eq($i + 1).fadeIn($transition_time);
-                slides().eq($i + 1).addClass('active');
-            }, $transition_time + $time_between_slides);
-    });
 
     addState(0);
 
@@ -50,3 +18,18 @@ window.onload = function() {
     tapeBoxUpdate(tapeBox);
     drawTuringMachine();
 };
+
+$(document).ready(function() {
+    $("#owl-demo").owlCarousel({
+        navigation: true, // Show next and prev buttons
+        slideSpeed: 300,
+        paginationSpeed: 400,
+        singleItem: true
+            // "singleItem:true" is a shortcut for:
+            // items : 1, 
+            // itemsDesktop : false,
+            // itemsDesktopSmall : false,
+            // itemsTablet: false,
+            // itemsMobile : false
+    });
+});
