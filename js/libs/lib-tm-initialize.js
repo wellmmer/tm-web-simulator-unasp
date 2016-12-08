0cc7a06e313cf9615b440a42dcae3e23bb8a5a7a
wellmmeR <wellmmer@gmail.com>
2016-12-08 01:00:56 -0200

    Alterações no Carousel

--- js/libs/lib-tm-initialize.js
+++ js/libs/lib-tm-initialize.js
@@ -16,16 +16,7 @@ window.onload = function() {
 };
 
 $(document).ready(function() {
-    $('#owl-demo').owlCarousel({
-        navigation: true, // Show next and prev buttons
-        slideSpeed: 300,
-        paginationSpeed: 400,
-        singleItem: true
-            // "singleItem:true" is a shortcut for:
-            // items : 1, 
-            // itemsDesktop : false,
-            // itemsDesktopSmall : false,
-            // itemsTablet: false,
-            // itemsMobile : false
+    $('.carousel').carousel({
+        interval: false
     });
 });
